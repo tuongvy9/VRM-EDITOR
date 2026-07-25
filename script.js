e476ff2c56b39a9a320cba9685478b265cd3c7ed
Tv <98216975+tuongvy9@users.noreply.github.com>
2026-07-26 02:40:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+import * as THREE from "./libs/three.module.js";
+import { OrbitControls } from "./libs/OrbitControls.js";
+import { GLTFLoader } from "./libs/GLTFLoader.js";
+import { VRMLoaderPlugin } from "./libs/three-vrm.module.js";
